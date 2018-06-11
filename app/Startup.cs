b9bf6da6ef9f72dbeff6b5a5eabece5dec291f1d
Aviran Mizrahi <AVIRANM@amdocs.com>
2018-06-11 11:06:38 +0300

    dsdsdsds

--- app/Startup.cs
+++ app/Startup.cs
@@ -22,6 +22,7 @@ namespace app
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
